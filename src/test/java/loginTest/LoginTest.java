d1cbe2012d98e36d66624b7bc31e0a88e30c31fa
dariatatarina <daryatatarina@gmail.com>
2019-12-29 01:52:28 +0200

    LoginTestRun

--- src/test/java/loginTest/LoginTest.java
+++ src/test/java/loginTest/LoginTest.java
@@ -1,6 +1,8 @@
 package loginTest;
 
+import org.junit.Assert;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -19,6 +21,16 @@ public class LoginTest {
         webDriver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
         webDriver.get ("https://www.brightlocal.com/");
 
+
+        webDriver.findElement(By.linkText ("Log in")).click();
+        webDriver.get ("https://tools.brightlocal.com/seo-tools/admin/login?redirect_url=%252Fseo-tools%252Fadmin%252Fclients-and-locations&redirect_source=access_control");
+        webDriver.findElement(By.name ("email")).clear();
+        webDriver.findElement(By.name("email")).sendKeys("dtautotest@gmail.com");
+
+        webDriver.findElement(By.name("password")).clear();
+        webDriver.findElement(By.name("password")).sendKeys("");
+
+        webDriver.findElement(By.tagName("button")).click();
 
         webDriver.quit();
 
